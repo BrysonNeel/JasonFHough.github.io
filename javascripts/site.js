e77f96665c2f6b493433fb7d19798d78de4859da
Jason Hough <jasonhough0@gmail.com>
2017-10-05 20:11:58 -0500

    Automated commit at 2017-10-06 01:11:58 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,49 @@
+var min = .75; //Keep this variable public for toggleDarkLight to access it
+function scrollingFade () { //The scrolling fade is in a function of its own to call it on page load, and when switching dark/light mode
+  /* Fading between divs on scroll */
+  $(window).scroll(function(){fading(true);});
+  function fading(pageLoad) {
+    var windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
+    var max=1, threshold=0.01;
+
+    $(".fading").each(function() {
+      /* Check the location of each desired element */
+      var objectHeight=$(this).outerHeight(), objectTop=$(this).offset().top, objectBottom=$(this).offset().top+objectHeight;
+
+      /* Fade element in/out based on its visible percentage */
+      if (objectTop < windowTop) {
+        if (objectBottom > windowTop) {$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/objectHeight)));}
+        else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
+      } else if (objectBottom > windowBottom) {
+        if (objectTop < windowBottom) {$(this).fadeTo(0,min+((max-min)*((windowBottom-objectTop)/objectHeight)));}
+        else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
+      } else if ($(this).css("opacity")<=max-threshold || pageLoad) {$(this).fadeTo(0,max);}
+    });
+  } fading(true); //fade elements on page-load
+  $(window).scroll(function(){fading(false);}); //fade elements on scroll
+}
+
+$(window).on("load",function() {
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  scrollingFade();
+});
+
+/* Dark Mode Toggle */
+function toggleDarkLight() {
+  //Change the CSS styling
+  var coloredRow = document.getElementById("body");
+  var currentClass = coloredRow.className;
+  coloredRow.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+
+  //Changes the button's text to reflect the mode change
+  if (currentClass == "dark-mode") {
+    buttonText = "Switch to Dark Mode";
+    min = .75;
+    scrollingFade();
+  } else {
+    buttonText = "Switch to Light Mode";
+    min = .9;
+    scrollingFade();
+  }
+  document.getElementById("dark_light").innerText = buttonText;
+}
